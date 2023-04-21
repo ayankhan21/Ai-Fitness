7fd716d98b993359c764f5a3d7227618ebc61d9d
ayankhan21 <ayankhanxyz00@gmail.com>
2023-04-22 03:04:30 +0530

    Integrated OpenAI api in the backend

--- Back/app.js
+++ Back/app.js
@@ -1,26 +1,96 @@
-const express = require('express')
-const app = express()
-const mongoose = require("mongoose");
-const Data = require('./model/data')
+import {Configuration , OpenAIApi} from "openai"
+import Express from 'express';
+import cors from 'cors'
+// const bodyParser = require('body-parser');
+import bodyParser from "body-parser";
+
+const app = Express();
+
+// Parse JSON request bodies
+app.use(bodyParser.json());
+app.use(cors())
+
+const openai = new OpenAIApi(new Configuration({
+  apiKey:'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',
+}))
 
 
-const dbURI =
-  "mongodb+srv://ayankhan21:Dellinspiron7588@cluster0.sfa1ovo.mongodb.net/interview-prep?retryWrites=true&w=majority";
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }) // for connection of DB; second arg not compulsory(only to avoid a warning)
-  .then((result) => {
-    // console.log(result)
-  })
-  .catch((err) => {
-    console.log(err);
+// function chatGpt(mes){
+//   let result;
+//   openai.createChatCompletion({
+//     model:"gpt-3.5-turbo",
+//     messages:[{role:"user", content:mes}]
+//   }).then(res=>{
+//     result = res.data.choices[0].message.content
+//     console.log(res.data.choices[0].message.content)
+//   }).finally(()=>{
+//     console.log("byee")
+//   })
+//   return result;
+// }
+
+function chatGpt(userRequest) {
+  let result;
+  return new Promise((resolve, reject) => {
+    openai.createChatCompletion({
+          model:"gpt-3.5-turbo",
+          messages:[{role:"user", content:userRequest}]
+        }).then(res=>{
+          result = res.data.choices[0].message.content
+          console.log(result)
+          resolve(result);
+        }).finally(()=>{
+          console.log("byee")
+        })
   });
+}
+
+// chatGpt('Design a course on the topic React , it should contain atleast 5 sections such as intro , Principles , a few more important sections and a 10 question MCQ questionaire')
 
 
+app.get('/',(req,res)=>{
+  res.send({message:"hello"})
+})
+
+
+// app.post('/',(req,res)=>{
+//     // console.log(req.body.data)
+//     let userRequest = req.body.data;
+//     const result = chatGpt(userRequest)
+//     console.log(typeof(result))
+//     res.send(result)
+// })
+
+app.post('/', async (req, res) => {
+  try {
+    const userRequest = req.body.data;
+    const result = await chatGpt(userRequest);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 
 
 app.listen(8000,()=>{
-    console.log('server is running on 3000')
+    console.log('server is running on 8000')
 })
+
+// const mongoose = require("mongoose");
+// const Data = require('./model/data')
+
+
+// const dbURI =
+//   "mongodb+srv://ayankhan21:Dellinspiron7588@cluster0.sfa1ovo.mongodb.net/interview-prep?retryWrites=true&w=majority";
+// mongoose.set("strictQuery", true);
+// mongoose
+//   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }) // for connection of DB; second arg not compulsory(only to avoid a warning)
+//   .then((result) => {
+//     // console.log(result)
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,44 @@
-import './App.css'
+import "./App.css";
+import axios from 'axios'
+import React, { useState } from 'react';
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [result,setResult] = useState("");
+  const [loading,setLoading] = useState(true)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const inputValue = event.target.elements.message.value;
+    setMessage(inputValue);
+    setLoading(true);
+    try {
+      console.log(message,"USER MESSAGE")
+      const response = await axios.post('http://localhost:8000', { data: message });
+      while (!response.data) {
+        await new Promise(resolve => setTimeout(resolve, 10000)); // wait 1 second before checking again
+      }
+      setResult(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+  // console.log(result)
+
   return (
     <div className="App">
-        <h1>INTERVIEW PREP</h1>
+      <h1>YOUR VERY OWN CHATGPT INTEGRATION</h1>
+      <form onSubmit={handleSubmit} action="">
+        <input type="text" name="message"/>
+        <button type="submit">Get</button>
+      </form>
+      <p>{message}</p>
+      {loading && <div>Loading...</div>}
+      {result && <div>{result}</div>}
     </div>
   );
 }
 
 export default App;
+
