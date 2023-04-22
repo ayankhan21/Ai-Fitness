f48f1c4974fa23cf2a3112a1b605f21faef492e2
ayankhan21 <ayankhanxyz00@gmail.com>
2023-04-22 09:12:21 +0530

    Fixed Exposed API KEY

--- Back/app.js
+++ Back/app.js
@@ -1,7 +1,8 @@
 import {Configuration , OpenAIApi} from "openai"
 import Express from 'express';
 import cors from 'cors'
-// const bodyParser = require('body-parser');
+import dotenv from 'dotenv'
+dotenv.config()
 import bodyParser from "body-parser";
 
 const app = Express();
@@ -11,23 +12,9 @@ app.use(bodyParser.json());
 app.use(cors())
 
 const openai = new OpenAIApi(new Configuration({
-  apiKey:'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',
+  apiKey:process.env.API_KEY,
 }))
 
-
-// function chatGpt(mes){
-//   let result;
-//   openai.createChatCompletion({
-//     model:"gpt-3.5-turbo",
-//     messages:[{role:"user", content:mes}]
-//   }).then(res=>{
-//     result = res.data.choices[0].message.content
-//     console.log(res.data.choices[0].message.content)
-//   }).finally(()=>{
-//     console.log("byee")
-//   })
-//   return result;
-// }
 
 function chatGpt(userRequest) {
   let result;
@@ -45,21 +32,11 @@ function chatGpt(userRequest) {
   });
 }
 
-// chatGpt('Design a course on the topic React , it should contain atleast 5 sections such as intro , Principles , a few more important sections and a 10 question MCQ questionaire')
-
 
 app.get('/',(req,res)=>{
   res.send({message:"hello"})
 })
 
-
-// app.post('/',(req,res)=>{
-//     // console.log(req.body.data)
-//     let userRequest = req.body.data;
-//     const result = chatGpt(userRequest)
-//     console.log(typeof(result))
-//     res.send(result)
-// })
 
 app.post('/', async (req, res) => {
   try {
@@ -73,24 +50,6 @@ app.post('/', async (req, res) => {
 });
 
 
-
-
 app.listen(8000,()=>{
     console.log('server is running on 8000')
 })
-
-// const mongoose = require("mongoose");
-// const Data = require('./model/data')
-
-
-// const dbURI =
-//   "mongodb+srv://ayankhan21:Dellinspiron7588@cluster0.sfa1ovo.mongodb.net/interview-prep?retryWrites=true&w=majority";
-// mongoose.set("strictQuery", true);
-// mongoose
-//   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }) // for connection of DB; second arg not compulsory(only to avoid a warning)
-//   .then((result) => {
-//     // console.log(result)
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
